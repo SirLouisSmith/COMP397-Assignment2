25c6a3d7fb87b1b509a2fb7fb9caccb0cb5a64dc
SirLouisSmith <someonewholives@hotmail.com>
2015-02-23 14:42:52 -0500

    Added Icons, Button Funtions, and Slot Containers

--- COMP397-Assignment2/Scripts/game.js
+++ COMP397-Assignment2/Scripts/game.js
@@ -1,7 +1,71 @@
 ﻿// CreateJS Boilerplate for COMP397
+//BUTTON CLASS
+var Button = (function () {
+    function Button(path, x, y) {
+        this._x = x;
+        this._y = y;
+        this._image = new createjs.Bitmap(path);
+        this._image.x = this._x;
+        this._image.y = this._y;
+
+        this._image.addEventListener("mouseover", this._buttonOver);
+        this._image.addEventListener("mouseout", this._buttonOut);
+    }
+    Button.prototype.setX = function (x) {
+        this._x = x;
+    };
+
+    Button.prototype.getX = function () {
+        return this._x;
+    };
+
+    Button.prototype.setY = function (y) {
+        this._y = y;
+    };
+
+    Button.prototype.getY = function () {
+        return this._y;
+    };
+
+    Button.prototype.getImage = function () {
+        return this._image;
+    };
+
+    Button.prototype._buttonOut = function (event) {
+        event.currentTarget.alpha = 1; // 100% Alpha
+    };
+
+    Button.prototype._buttonOver = function (event) {
+        event.currentTarget.alpha = 0.7;
+    };
+    return Button;
+})();
+
 // VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 var canvas;
 var stage;
+var icons = [];
+var slotContainer = [];
+
+// GAME VARIABLES
+var playerMoney = 1000;
+var winnings = 0;
+var jackpot = 5000;
+var turn = 0;
+var playerBet = 0;
+var winNumber = 0;
+var lossNumber = 0;
+var spinResult;
+var tmnt = "";
+var winRatio = 0;
+var leo = 0;
+var michel = 0;
+var raph = 0;
+var doni = 0;
+var splinter = 0;
+var april = 0;
+var pizza = 0;
+var shredder = 0;
 
 // GAME OBJECTS
 var game;
@@ -11,6 +75,7 @@ var betMaxButton;
 var betOneButton;
 var resetButton;
 var powerButton;
+var jackpotTxt;
 
 // FUNCTIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function init() {
@@ -29,56 +94,187 @@ function gameLoop() {
     stage.update();
 }
 
+function resetTally() {
+    leo = 0;
+    michel = 0;
+    raph = 0;
+    doni = 0;
+    splinter = 0;
+    april = 0;
+    pizza = 0;
+    shredder = 0;
+}
+
+function resetAll() {
+    playerMoney = 1000;
+    winnings = 0;
+    jackpot = 5000;
+    turn = 0;
+    playerBet = 0;
+    winNumber = 0;
+    lossNumber = 0;
+    winRatio = 0;
+}
+
+/* Utility function to check if a value falls within a range of bounds */
+function checkRange(value, lowerBounds, upperBounds) {
+    if (value >= lowerBounds && value <= upperBounds) {
+        return value;
+    } else {
+        return !value;
+    }
+}
+
+function Reels() {
+    var betLine = [" ", " ", " "];
+    var outCome = [0, 0, 0];
+
+    for (var spin = 0; spin < 3; spin++) {
+        outCome[spin] = Math.floor((Math.random() * 65) + 1);
+        switch (outCome[spin]) {
+            case checkRange(outCome[spin], 1, 27):
+                betLine[spin] = "shredder";
+                shredder++;
+                break;
+            case checkRange(outCome[spin], 28, 37):
+                betLine[spin] = "leo";
+                leo++;
+                break;
+            case checkRange(outCome[spin], 38, 46):
+                betLine[spin] = "michel";
+                michel++;
+                break;
+            case checkRange(outCome[spin], 47, 54):
+                betLine[spin] = "raph";
+                raph++;
+                break;
+            case checkRange(outCome[spin], 55, 59):
+                betLine[spin] = "doni";
+                doni++;
+                break;
+            case checkRange(outCome[spin], 60, 62):
+                betLine[spin] = "splinter";
+                splinter++;
+                break;
+            case checkRange(outCome[spin], 63, 64):
+                betLine[spin] = "april";
+                april++;
+                break;
+            case checkRange(outCome[spin], 65, 65):
+                betLine[spin] = "pizza";
+                pizza++;
+                break;
+        }
+    }
+    return betLine;
+}
+
+function determineWinnings() {
+    if (shredder == 0) {
+        if (leo == 3) {
+            winnings = playerBet * 10;
+        } else if (michel == 3) {
+            winnings = playerBet * 20;
+        } else if (raph == 3) {
+            winnings = playerBet * 30;
+        } else if (doni == 3) {
+            winnings = playerBet * 40;
+        } else if (splinter == 3) {
+            winnings = playerBet * 50;
+        } else if (april == 3) {
+            winnings = playerBet * 75;
+        } else if (pizza == 3) {
+            winnings = playerBet * 100;
+        } else if (leo == 2) {
+            winnings = playerBet * 2;
+        } else if (michel == 2) {
+            winnings = playerBet * 2;
+        } else if (raph == 2) {
+            winnings = playerBet * 3;
+        } else if (doni == 2) {
+            winnings = playerBet * 4;
+        } else if (splinter == 2) {
+            winnings = playerBet * 5;
+        } else if (april == 2) {
+            winnings = playerBet * 10;
+        } else if (pizza == 2) {
+            winnings = playerBet * 20;
+        } else if (pizza == 1) {
+            winnings = playerBet * 5;
+        } else {
+            winnings = playerBet * 1;
+        }
+        winNumber++;
+        //showWinMessage();
+    } else {
+        lossNumber++;
+        //showLossMessage();
+    }
+}
+
 function spinButtonClicked() {
-    console.log("Spin Button Clicked");
+    spinResult = Reels();
+    tmnt = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+
+    for (var slot = 0; slot < slotContainer.length; slot++) {
+        slotContainer[slot].removeAllChildren();
+        icons[slot] = new createjs.Bitmap("assets/images/" + spinResult[slot] + ".png");
+        slotContainer[slot].addChild(icons[slot]);
+    }
+    jackpotTxt.text = "$" + jackpot;
 }
 
-function spinButtonOut() {
-    spinButton.alpha = 1; // 100% Alpha
+function ButtonOut(event) {
+    event.currentTarget.alpha = 1; // 100% Alpha
 }
 
-function spinButtonOver() {
-    spinButton.alpha = 0.7;
+function ButtonOver(event) {
+    event.currentTarget.alpha = 0.7;
 }
 
 function createUI() {
     background = new createjs.Bitmap("assets/images/background.png");
     game.addChild(background);
 
+    for (var index = 0; index < 3; index++) {
+        slotContainer[index] = new createjs.Container();
+        game.addChild(slotContainer[index]);
+    }
+
+    slotContainer[0].x = 40.85;
+    slotContainer[0].y = 132;
+    slotContainer[1].x = 208.85;
+    slotContainer[1].y = 132;
+    slotContainer[2].x = 376.85;
+    slotContainer[2].y = 132;
+
     // Spin Button
-    spinButton = new createjs.Bitmap("assets/images/spinButton.png");
-    game.addChild(spinButton);
-    spinButton.x = 179;
-    spinButton.y = 411;
+    spinButton = new Button("assets/images/spinButton.png", 179, 411);
+    game.addChild(spinButton.getImage());
 
     // Spin Button Event Listeners
-    spinButton.addEventListener("click", spinButtonClicked);
-    spinButton.addEventListener("mouseover", spinButtonOver);
-    spinButton.addEventListener("mouseout", spinButtonOut);
+    spinButton.getImage().addEventListener("click", spinButtonClicked);
 
     // Bet Max Button
-    betMaxButton = new createjs.Bitmap("assets/images/betMaxButton.png");
-    game.addChild(betMaxButton);
-    betMaxButton.x = 442;
-    betMaxButton.y = 509;
+    betMaxButton = new Button("assets/images/betMaxButton.png", 442, 509);
+    game.addChild(betMaxButton.getImage());
 
     // Bet One Button
-    betOneButton = new createjs.Bitmap("assets/images/betOneButton.png");
-    game.addChild(betOneButton);
-    betOneButton.x = 43;
-    betOneButton.y = 509;
+    betOneButton = new Button("assets/images/betOneButton.png", 43, 509);
+    game.addChild(betOneButton.getImage());
 
     // Reset Button
-    resetButton = new createjs.Bitmap("assets/images/resetButton.png");
-    game.addChild(resetButton);
-    resetButton.x = 135;
-    resetButton.y = 365;
+    resetButton = new Button("assets/images/resetButton.png", 135, 365);
+    game.addChild(resetButton.getImage());
 
     // Power Button
-    powerButton = new createjs.Bitmap("assets/images/powerButton.png");
-    game.addChild(powerButton);
-    powerButton.x = 368;
-    powerButton.y = 365;
+    powerButton = new Button("assets/images/powerButton.png", 368, 365);
+    game.addChild(powerButton.getImage());
+
+    jackpotTxt = new createjs.Text("$" + jackpot, "26px Arial", "#ffffff");
+    game.addChild(jackpotTxt);
+    jackpotTxt.x = 280;
+    jackpotTxt.y = 22;
 }
 
 function main() {
--- COMP397-Assignment2/Scripts/game.ts
+++ COMP397-Assignment2/Scripts/game.ts
@@ -1,19 +1,88 @@
 ﻿// CreateJS Boilerplate for COMP397
 
+//BUTTON CLASS
+class Button {
+    //private variables
+    private _image: createjs.Bitmap;
+    private _x: number;
+    private _y: number;
+    constructor(path:string, x:number, y:number) {
+        this._x = x;
+        this._y = y;
+        this._image = new createjs.Bitmap(path);
+        this._image.x = this._x;
+        this._image.y = this._y;
+
+        this._image.addEventListener("mouseover", this._buttonOver);
+        this._image.addEventListener("mouseout", this._buttonOut);
+    }
+
+    public setX(x: number): void {
+        this._x = x;
+    }
+
+    public getX(): number {
+        return this._x;
+    }
+
+    public setY(y: number): void {
+        this._y = y;
+    }
+
+    public getY(): number {
+        return this._y;
+    }
+
+    public getImage(): createjs.Bitmap {
+        return this._image;
+    }
+
+    private _buttonOut(event: createjs.MouseEvent): void {
+        event.currentTarget.alpha = 1; // 100% Alpha 
+
+    }
+
+    private _buttonOver(event: createjs.MouseEvent): void {
+        event.currentTarget.alpha = 0.7;
+
+    }
+}
 
 // VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 var canvas; // Reference to the HTML 5 Canvas element
 var stage: createjs.Stage; // Reference to the Stage
+var icons: createjs.Bitmap[] = [];
+var slotContainer: createjs.Container[] = [];
 
+// GAME VARIABLES
+var playerMoney = 1000;
+var winnings = 0;
+var jackpot = 5000;
+var turn = 0;
+var playerBet = 0;
+var winNumber = 0;
+var lossNumber = 0;
+var spinResult;
+var tmnt = "";
+var winRatio = 0;
+var leo = 0;
+var michel = 0;
+var raph = 0;
+var doni = 0;
+var splinter = 0;
+var april = 0;
+var pizza = 0;
+var shredder = 0;
 
 // GAME OBJECTS
 var game: createjs.Container; // Main Game Container Object
 var background: createjs.Bitmap;
-var spinButton: createjs.Bitmap;
-var betMaxButton: createjs.Bitmap;
-var betOneButton: createjs.Bitmap;
-var resetButton: createjs.Bitmap;
-var powerButton: createjs.Bitmap;
+var spinButton: Button;
+var betMaxButton: Button;
+var betOneButton: Button;
+var resetButton: Button;
+var powerButton: Button;
+var jackpotTxt: createjs.Text;
 
 
 // FUNCTIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -37,17 +106,162 @@ function gameLoop() {
     stage.update();
 }
 
+function resetTally() {
+    leo = 0;
+    michel = 0;
+    raph = 0;
+    doni = 0;
+    splinter = 0;
+    april = 0;
+    pizza = 0;
+    shredder = 0;
+}
+
+function resetAll() {
+    playerMoney = 1000;
+    winnings = 0;
+    jackpot = 5000;
+    turn = 0;
+    playerBet = 0;
+    winNumber = 0;
+    lossNumber = 0;
+    winRatio = 0;
+}
+
+/* Utility function to check if a value falls within a range of bounds */
+function checkRange(value, lowerBounds, upperBounds) {
+    if (value >= lowerBounds && value <= upperBounds) {
+        return value;
+    }
+    else {
+        return !value;
+    }
+}
+
+function Reels() {
+    var betLine = [" ", " ", " "];
+    var outCome = [0, 0, 0];
+
+    for (var spin = 0; spin < 3; spin++) {
+        outCome[spin] = Math.floor((Math.random() * 65) + 1);
+        switch (outCome[spin]) {
+            case checkRange(outCome[spin], 1, 27):  // 41.5% probability
+                betLine[spin] = "shredder";
+                shredder++;
+                break;
+            case checkRange(outCome[spin], 28, 37): // 15.4% probability
+                betLine[spin] = "leo";
+                leo++;
+                break;
+            case checkRange(outCome[spin], 38, 46): // 13.8% probability
+                betLine[spin] = "michel";
+                michel++;
+                break;
+            case checkRange(outCome[spin], 47, 54): // 12.3% probability
+                betLine[spin] = "raph";
+                raph++;
+                break;
+            case checkRange(outCome[spin], 55, 59): //  7.7% probability
+                betLine[spin] = "doni";
+                doni++;
+                break;
+            case checkRange(outCome[spin], 60, 62): //  4.6% probability
+                betLine[spin] = "splinter";
+                splinter++;
+                break;
+            case checkRange(outCome[spin], 63, 64): //  3.1% probability
+                betLine[spin] = "april";
+                april++;
+                break;
+            case checkRange(outCome[spin], 65, 65): //  1.5% probability
+                betLine[spin] = "pizza";
+                pizza++;
+                break;
+        }
+    }
+    return betLine;
+}
+
+function determineWinnings() {
+    if (shredder == 0) {
+        if (leo == 3) {
+            winnings = playerBet * 10;
+        }
+        else if (michel == 3) {
+            winnings = playerBet * 20;
+        }
+        else if (raph == 3) {
+            winnings = playerBet * 30;
+        }
+        else if (doni == 3) {
+            winnings = playerBet * 40;
+        }
+        else if (splinter == 3) {
+            winnings = playerBet * 50;
+        }
+        else if (april == 3) {
+            winnings = playerBet * 75;
+        }
+        else if (pizza == 3) {
+            winnings = playerBet * 100;
+        }
+        else if (leo == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (michel == 2) {
+            winnings = playerBet * 2;
+        }
+        else if (raph == 2) {
+            winnings = playerBet * 3;
+        }
+        else if (doni == 2) {
+            winnings = playerBet * 4;
+        }
+        else if (splinter == 2) {
+            winnings = playerBet * 5;
+        }
+        else if (april == 2) {
+            winnings = playerBet * 10;
+        }
+        else if (pizza == 2) {
+            winnings = playerBet * 20;
+        }
+        else if (pizza == 1) {
+            winnings = playerBet * 5;
+        }
+        else {
+            winnings = playerBet * 1;
+        }
+        winNumber++;
+        //showWinMessage();
+    }
+    else {
+        lossNumber++;
+        //showLossMessage();
+    }
+
+}
+
 function spinButtonClicked() {
-    console.log("Spin Button Clicked");
+
+    spinResult = Reels();
+    tmnt = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+
+    for (var slot = 0; slot < slotContainer.length; slot++) {
+        slotContainer[slot].removeAllChildren();
+        icons[slot] = new createjs.Bitmap("assets/images/" + spinResult[slot] + ".png");
+        slotContainer[slot].addChild(icons[slot]);
+    }
+    jackpotTxt.text = "$" + jackpot;
 }
 
-function spinButtonOut() {
-    spinButton.alpha = 1; // 100% Alpha 
+function ButtonOut(event: createjs.MouseEvent) {
+    event.currentTarget.alpha = 1; // 100% Alpha 
 
 }
 
-function spinButtonOver() {
-    spinButton.alpha = 0.7;
+function ButtonOver(event: createjs.MouseEvent) {
+    event.currentTarget.alpha = 0.7;
 
 }
 
@@ -56,41 +270,45 @@ function createUI() {
     background = new createjs.Bitmap("assets/images/background.png");
     game.addChild(background);
 
+    for (var index = 0; index < 3; index++) {
+        slotContainer[index] = new createjs.Container();
+        game.addChild(slotContainer[index]);
+    }
+
+    slotContainer[0].x = 40.85;
+    slotContainer[0].y = 132;
+    slotContainer[1].x = 208.85;
+    slotContainer[1].y = 132;
+    slotContainer[2].x = 376.85;
+    slotContainer[2].y = 132;
+
     // Spin Button
-    spinButton = new createjs.Bitmap("assets/images/spinButton.png");
-    game.addChild(spinButton);
-    spinButton.x = 179;
-    spinButton.y = 411;
+    spinButton = new Button("assets/images/spinButton.png", 179, 411);
+    game.addChild(spinButton.getImage());
 
     // Spin Button Event Listeners
-    spinButton.addEventListener("click", spinButtonClicked);
-    spinButton.addEventListener("mouseover", spinButtonOver);
-    spinButton.addEventListener("mouseout", spinButtonOut);
-
+    spinButton.getImage().addEventListener("click", spinButtonClicked);
 
     // Bet Max Button
-    betMaxButton = new createjs.Bitmap("assets/images/betMaxButton.png");
-    game.addChild(betMaxButton);
-    betMaxButton.x = 442;
-    betMaxButton.y = 509;
+    betMaxButton = new Button("assets/images/betMaxButton.png", 442, 509);
+    game.addChild(betMaxButton.getImage());
 
     // Bet One Button
-    betOneButton = new createjs.Bitmap("assets/images/betOneButton.png");
-    game.addChild(betOneButton);
-    betOneButton.x = 43;
-    betOneButton.y = 509;
+    betOneButton = new Button("assets/images/betOneButton.png", 43, 509);
+    game.addChild(betOneButton.getImage());
 
     // Reset Button
-    resetButton = new createjs.Bitmap("assets/images/resetButton.png");
-    game.addChild(resetButton);
-    resetButton.x = 135;
-    resetButton.y = 365;
+    resetButton = new Button("assets/images/resetButton.png", 135, 365);
+    game.addChild(resetButton.getImage());
 
     // Power Button
-    powerButton = new createjs.Bitmap("assets/images/powerButton.png");
-    game.addChild(powerButton);
-    powerButton.x = 368;
-    powerButton.y = 365;
+    powerButton = new Button("assets/images/powerButton.png", 368, 365);
+    game.addChild(powerButton.getImage());
+
+    jackpotTxt = new createjs.Text("$" + jackpot, "26px Arial", "#ffffff");
+    game.addChild(jackpotTxt);
+    jackpotTxt.x = 280;
+    jackpotTxt.y = 22;
 }
 
 function main() {
